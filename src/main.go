f0294adc25e3d009f4476dc355a00ceb576ec09b
blancazure <jblanco@blancazure.com>
2024-08-08 16:51:30 +0200

    Vuelta al código original por errores

--- src/main.go
+++ src/main.go
@@ -1,45 +1,29 @@
 package main
 
 import (
+    "html/template"
     "log"
     "net/http"
     "os"
-    "os/signal"
-    "syscall"
+    "github.com/gorilla/mux"
 )
 
+var templates = template.Must(template.ParseFiles("templates/index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    templates.ExecuteTemplate(w, "index.html", nil)
+}
+
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    http.HandleFunc("/templates/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
-    })
+    r := mux.NewRouter()
+    r.HandleFunc("/", indexHandler)
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "3000"
+    puerto := os.Getenv("PUERTO_TETRIS")
+    if puerto == "" {
+        puerto = "3000"
     }
 
-    server := &http.Server{Addr: ":" + port}
-
-    // Canal para recibir las señales del sistema
-    stop := make(chan os.Signal, 1)
-    signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-
-    // Iniciar el servidor en una goroutine
-    go func() {
-        log.Println("Iniciando servidor en el puerto " + port)
-        if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("Error en ListenAndServe: %v\n", err)
-        }
-    }()
-
-    // Esperar una señal de parada
-    <-stop
-
-    // Realizar una parada limpia del servidor
-    log.Println("Deteniendo servidor...")
-    if err := server.Close(); err != nil {
-        log.Fatalf("Error en la parada del servidor: %v\n", err)
-    }
-    log.Println("Servidor detenido.")
+    log.Println("Servidor iniciado en el puerto:", puerto)
+    log.Fatal(http.ListenAndServe(":"+puerto, r))
 }
